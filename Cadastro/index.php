02edf8547d69032eca9f854441264b7ea4bbe925
Jefferson Mateus <jeffersonmateus64@gmail.com>
2020-12-18 16:39:02 -0300

    Telas do sistema

--- Cadastro/index.php
+++ Cadastro/index.php
@@ -6,7 +6,7 @@ require ("config/crud.php");
 <html>
     <head>
         <meta charset="utf-8">
-        <title>mjailton</title>
+        <title>Cadastro de clientes</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
